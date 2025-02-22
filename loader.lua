c5a38e5923447c08f130c7482758b3946194180e
imgoated2352345213 <ilovecarts391@gmail.com>
2025-02-22 15:33:02 -0500

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,6 @@
+if game["GameId"] == 113491250 then
+game:GetService("Players")["LocalPlayer"]:Kick("Da Hood is not supported at this time.")
+return
+end
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/f5cfd7f131473a4b567caed767725cde.lua"))()
