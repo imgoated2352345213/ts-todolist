0035ad51379914cd0dd272396086d26651b0129e
imgoated2352345213 <ilovecarts391@gmail.com>
2025-02-28 13:50:33 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,4 +1,4 @@
-if game["GameId"] == 113491250 then
+if game["GameId"] == 1008451066 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ddb9688b49c85c6e994df475d70b39fd.lua"))()
 else
     game:GetService("Players")["LocalPlayer"]:Kick("This game is not supported at this time.")
