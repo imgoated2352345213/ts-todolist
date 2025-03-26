237cd83c9bc58b7051781ab2bb890d3d857d487b
imgoated2352345213 <ilovecarts391@gmail.com>
2025-03-26 02:09:32 -0400

    Update beta.lua

--- beta.lua
+++ beta.lua
@@ -6,4 +6,4 @@ if executor == "Solara" or executor == "Xeno" or executor == "Delta" or executor
 end
 
 local id = game["GameId"]
-loadstring(game:HttpGet(id == 1008451066 and "https://api.luarmor.net/files/v3/loaders/c58d9d41abab684038043328ee801418.lua" or "https://api.luarmor.net/files/v3/loaders/5c67cd08a8565b5aee8af935821fdddf.lua"))()
+loadstring(game:HttpGet(id == 1008451066 and "https://api.luarmor.net/files/v3/loaders/c58d9d41abab684038043328ee801418.lua" or "https://api.luarmor.net/files/v3/loaders/2c295ea5f3e843012dfd6cd2a3231300.lua"))()
